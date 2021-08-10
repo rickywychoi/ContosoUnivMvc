7f5f7bba64df15006943e0e0a6b8952a3ccc826e
Ricky Choi <rchoi@cadmakers.com>
2021-08-10 14:55:54 -0700

    Up to scaffolding Students view and controller

---
+++ Data/SchoolContext.cs
@@ -0,0 +1,28 @@
+﻿using ContosoUniversity.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Course>().ToTable("Course");
+            builder.Entity<Enrollment>().ToTable("Enrollment");
+            builder.Entity<Student>().ToTable("Student");
+        }
+    }
+}
---
+++ Models/Enrollment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    public enum Grade
+    {
+        A, B, C, D, F
+    }
+
+    public class Enrollment
+    {
+        public int Id { get; set; }
+        public Grade? Grade { get; set; }
+        public int CourseId { get; set; }
+        public int StudentId { get; set; }
+
+        public Course Course { get; set; }
+        public Student Student { get; set; }
+    }
+}
